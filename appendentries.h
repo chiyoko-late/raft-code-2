e035be5bb06d1e8c855910ac9989c96b6aa85b09
chiyoko_late <hajime96suke@gmail.com>
2023-02-06 17:09:43 +0900

    1000L*10

--- appendentries.h
+++ appendentries.h
@@ -15,7 +15,7 @@
 #include "my_sock.h"
 
 #define SERVER_ADDR "0.0.0.0"
-#define STRING_MAX (100L)
+#define STRING_MAX (1000L*10)
 #define ALL_ACCEPTED_ENTRIES (50L)
 #define ONCE_SEND_ENTRIES (5L)
 
