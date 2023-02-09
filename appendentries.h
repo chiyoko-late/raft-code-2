1a171d98d11d82da1f4210170297e708c1166579
chiyoko-late <93936783+chiyoko-late@users.noreply.github.com>
2023-02-09 17:08:20 +0900

    Update appendentries.h

--- appendentries.h
+++ appendentries.h
@@ -15,9 +15,9 @@
 #include "my_sock.h"
 
 #define SERVER_ADDR "0.0.0.0"
-#define STRING_MAX (1000L*10000)
-#define ALL_ACCEPTED_ENTRIES (1000L)
-#define ONCE_SEND_ENTRIES (100L)
+#define STRING_MAX (1000L*1000)
+#define ALL_ACCEPTED_ENTRIES (5000L)
+#define ONCE_SEND_ENTRIES (500L)
 
 uint64_t c1,
     c2;
